0e7f45a4e99f87b2651fc4918c6bc497745a46d0
sharonbash26 <sharon.bash@live.biu.ac.il>
2023-10-06 03:20:32 +0300

    add scss design

--- vite-project/src/cmps/NavHeader.jsx
+++ vite-project/src/cmps/NavHeader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ImgUploader } from './ImgUploader';
 const { useState, useEffect } = React
 
-export function NavHeader({onAddPst}) {
+export function NavHeader({ onAddPst }) {
     const [isUploadModalOpen, setIsUploadModalOpen] = useState(false)
     function openModal() {
         setIsUploadModalOpen(true)
@@ -18,10 +18,10 @@ export function NavHeader({onAddPst}) {
 
             <section className="btn-container">
                 <button><img src="svgs_collection/svg1.svg"></img><span className="home-span">Home</span></button>
-                <button><img src="svgs_collection/svg2.svg"></img><span>Search</span></button>
-                <button><img src="svgs_collection/svg4.svg"></img><span>Explore</span></button>
-                <button><img src="svgs_collection/svg7.svg"></img><span>Reels</span></button>
-                <button><img src="svgs_collection/svg5.svg"></img><span>Messages</span></button>
+                <button><img src="search-icon-white.svg"></img><span>Search</span></button>
+                <button><img src="explore-white.svg"></img><span>Explore</span></button>
+                <button><img src="Reels.svg"></img><span>Reels</span></button>
+                <button><img src="mess.svg"></img><span>Messages</span></button>
                 <button><img src="svgs_collection/svg6.svg"></img><span>Notifications</span></button>
                 <button onClick={openModal}><i className="fa-regular fa-square-plus"></i>Create</button>
                 <button><img className="profile-icon-img" src="s3.jpg"></img>Profile </button>
@@ -31,14 +31,18 @@ export function NavHeader({onAddPst}) {
                     <div className="modal">
                         <div className="modal-content">
                             <h3>Create new post</h3>
-                            <h2>Drag photos and videos here</h2>
-                            <span className="close-button" onClick={closeModal}>&times;</span>
-                            <ImgUploader onUploaded={(url) => {
-                                console.log('urlFromNav',url)
-                                console.log('navvvvvvvvvvvv')
-                                onAddPst(url)
-                                closeModal();
-                            }} />
+                            <div className='inner-container'>
+                                <img className='icon-upload-background' src="upload-window.svg"></img>
+                                <h3>Drag photos and videos here</h3>
+                                <span className="close-button" onClick={closeModal}>&times;</span>
+                                <ImgUploader onUploaded={(url) => {
+                                    console.log('urlFromNav', url)
+                                    console.log('navvvvvvvvvvvv')
+                                    onAddPst(url)
+                                    closeModal();
+                                }} />
+                            </div>
+
                         </div>
                     </div>
                 )}
