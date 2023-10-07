c9b9703932349366d18fde66103a938fe1c3d09b
sharonbash26 <sharon.bash@live.biu.ac.il>
2023-10-08 02:29:37 +0300

    F/add emoji container and fix bug design

--- vite-project/src/cmps/NavHeader.jsx
+++ vite-project/src/cmps/NavHeader.jsx
@@ -18,18 +18,26 @@ export function NavHeader({ onAddPst }) {
             </div>
 
             <section className="btn-container">
-                <button className='item-first'><img src="svgs_collection/svg1.svg"></img><span className="home-span">Home</span></button>
-                <button className='item'><img src="search-icon-white.svg"></img><span>Search</span></button>
-                <button className='item'><img src="explore-white.svg"></img><span>Explore</span></button>
-                <button className='item'><img src="Reels.svg"></img><span>Reels</span></button>
-                <button className='item'><img src="mess.svg"></img><span>Messages</span></button>
-                <button className='item'><img src="svgs_collection/svg6.svg"></img><span>Notifications</span></button>
-                <button className='item' onClick={openModal}><img src="create.svg"></img>Create</button>
-                <button className='item'><img className="profile-icon-img" src="s3.jpg"></img>Profile </button>
-                <button className='item-down'><img src="svgs_collection/svg8.svg"></img>Threads</button>
-                <button className='item-more'><img src="svgs_collection/svg10.svg"></img>More</button>
-                {isUploadModalOpen && (<UploadModal closeModal={closeModal} onAddPst={onAddPst} />
-                )}
+             
+                    <div className='first'>
+                        <button className='item item-first'><img src="svgs_collection/svg1.svg"></img><span className="home-span">Home</span></button>
+                        <button className='item'><img src="search-icon-white.svg"></img><span>Search</span></button>
+                        <button className='item'><img src="explore-white.svg"></img><span>Explore</span></button>
+                        <button className='item'><img src="Reels.svg"></img><span>Reels</span></button>
+                        <button className='item'><img src="mess.svg"></img><span>Messages</span></button>
+                        <button className='item'><img src="svgs_collection/svg6.svg"></img><span>Notifications</span></button>
+                        <button className='item' onClick={openModal}><img src="create.svg"></img><span>Create</span></button>
+                        <button className='item'><img className="profile-icon-img" src="s3.jpg"></img><span>Profile</span> </button>
+                    </div>
+
+                    <div className='second'>
+                        <button className='item item-down'><img src="svgs_collection/svg8.svg"></img><span>Threads</span></button>
+                        <button className='item item-more'><img src="svgs_collection/svg10.svg"></img><span>More</span></button>
+                        {isUploadModalOpen && (<UploadModal closeModal={closeModal} onAddPst={onAddPst} />
+                        )}
+            
+
+                </div>
             </section>
         </section>
     )
--- vite-project/src/cmps/UploadModal.jsx
+++ vite-project/src/cmps/UploadModal.jsx
@@ -4,7 +4,8 @@ import React from 'react';
 export function UploadModal({ closeModal, onAddPst }) {
     return (
         <div className="modal">
-            <img className="cross-icon"src="cross.png" onClick={closeModal}></img>
+            {/* <img className="cross-icon" src="cross.png" onClick={closeModal}></img> */}
+            <svg onClick={closeModal} aria-label="Close" class="cross-icon" color="rgb(255, 255, 255)" fill="rgb(255, 255, 255)" height="18" role="img" viewBox="0 0 24 24" width="18"><title>Close</title><polyline fill="none" points="20.643 3.357 12 12 3.353 20.647" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"></polyline><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" x1="20.649" x2="3.354" y1="20.649" y2="3.354"></line></svg>
             {/* <span className="close-button" onClick={closeModal}>&times;</span> */}
             <div className="modal-content">
                 <h3>Create new post</h3>
